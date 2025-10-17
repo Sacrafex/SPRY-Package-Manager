5e763f1e156bb753c6190c0c2ec3b354909c5a12
Killian Zabinsky <sacrafexofficial@gmail.com>
2025-10-17 06:21:26 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+echo "==========================================="
+echo "        Installing SPRY Package Manager     "
+echo "==========================================="
+echo
+echo "[INFO] Starting installation..."
+echo "[INFO] Checking for latest version..."
+echo "[ERROR] Failed to install SPRY."
+echo "[CAUSE] Version v1.0.0 isn't available at the moment."
+echo
+echo "SPRY is still in development and not yet finished."
+echo "Please check back later."
+echo "==========================================="
